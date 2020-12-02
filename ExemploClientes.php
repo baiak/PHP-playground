9d799c5e652b0b6c8f165da489b25e91ccedeee8
baiak <jonathanbaiak@gmail.com>
2020-12-02 00:02:54 -0300

    Create ExemploClientes.php
    Exemplo de uso da classe de conexao, inserção e busca SQL com PHP/ PDO

---
+++ ExemploClientes.php
@@ -0,0 +1,46 @@
+<?php
+require("baseDados.php");
+//exemplo de uso da classe da base de dados
+
+
+class SearchClientes extends DB{
+    
+    //gera uma listbox com todos os clientes da base de dados
+public function listBox(){
+    $db = new DB();
+    $stmt = $db->conn->prepare("SELECT * FROM baiaksoft_fenixinfo2.cliente ORDER BY NOME ASC");
+    
+    $stmt->execute();
+    $arr = $stmt->fetchAll();
+
+    echo('<select name="cliente" id="cliente">');
+     foreach($arr as $key => $value){
+         echo('<option value="'.$value["idCLIENTE"].'">'.$value["NOME"].'</option>');
+     }
+     echo( '</select>'); 
+    }  
+
+
+    //carrega apenas um cliente, se nao existir, gera um listbox com todos os clientes
+public function tkCliente($ident){
+    $db = new DB();
+    $stmt= $db->conn->prepare("SELECT NOME FROM baiaksoft_fenixinfo2.cliente WHERE idCLIENTE = :ID_CLIENTE");
+    $stmt->bindValue(":ID_CLIENTE", $ident);
+    $stmt->execute();
+    $arr = $stmt->fetch();
+     
+    if($arr > 0){
+          
+          foreach($arr as $key => $value){
+              echo ($value);
+            } 
+    }else{
+        return ($this->listBox());
+    }
+ 
+  }
+
+
+}
+
+?>
